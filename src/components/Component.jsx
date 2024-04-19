059a07998aee48559daa773e7210d5ea84a87717
sribabu <sribabumandraju@gmail.com>
2024-04-19 15:40:19 +0530

    changes

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -7,7 +7,7 @@ const Component = () => {
     <>
         <div className='w-full h-screen bg-black flex items-center justify-center bg-dark flex-col'>
             <div className="text-white font-bold text-5xl">Write Your Code here</div>
-            <div className="flex items-center pt-5 text-white"><PiSmileyFill className="text-[50px] text-[green] pe-5" /> Happy Work <sub className="text-[aqua] text-[8px] ps-4">~ Sribabu</sub> </div>
+            <div className="flex items-center pt-5 text-white"><PiSmileyFill className="text-[50px] text-[green] pe-5" /> Happy Learing <sub className="text-[aqua] text-[8px] ps-4">~ Sribabu</sub> </div>
 
         </div>
     </>
