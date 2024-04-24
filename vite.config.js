72f4d42fed3ea90d841e8647b620f4d63b814df0
sribabu <sribabumandraju@gmail.com>
2024-04-24 21:51:01 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['swiper/react']
-    }
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './node_modules'),
-    },
-  },
+  
+
 })
